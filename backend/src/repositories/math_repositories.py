14b04b9fcd7b76c419608cbeaf6e46fbc4aa1a5a
GanduriKumar <ganduri.kumar@gmail.com>
2025-12-15 15:41:16 +0530

    Changes - deletion

--- backend/src/repositories/math_repositories.py
+++
@@ -1,9 +0,0 @@
-class MathRepository:
-    def __init__(self):
-        self.operations = []
-
-    def save_operation(self, operation):
-        self.operations.append(operation)
-
-    def get_operations(self):
-        return self.operations
